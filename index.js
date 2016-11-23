a97eceed492e717d07affbccf5312fb8ef37ebd4
Louiefigz <figueroa.aaron@gmail.com>
2016-11-23 13:42:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const app = "I don't do much.";
+
+
+//pure functions are functions that are meant to not change the origninal variable.
+
+//It safeguards against accidental  deleting or adding things to the database. 
